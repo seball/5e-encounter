8489e7a07da9dd042918cd95431bc53d2b6ebf4e
S3ball <sebastian.czubienko@gmail.coml>
2024-09-20 15:04:41 +0200

    mockup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CharacterComponent } from './character/character.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CharacterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '5e-encounter';
