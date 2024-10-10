249da4ed04337f5be4a30570142e5e7298b5b39d
S3ball <sebastian.czubienko@gmail.coml>
2024-10-10 19:58:04 +0200

    feat(animation): initial

--- src/app/components/battlefield/battlefield.component.ts
+++ src/app/components/battlefield/battlefield.component.ts
@@ -1,16 +1,9 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  signal,
-  computed,
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacterService } from '../../services/character.service';
-import { Character } from '../../interfaces/character.interface';
 import { CharacterComponent } from '../character/character.component';
 import { CreateCharacterComponent } from '../create-character/create-character.component';
 import { MonsterStatBlockComponent } from '../monster-stat-block/monster-stat-block.component';
-import { listAnimation } from './list-animation';
 
 @Component({
   selector: 'app-battlefield',
@@ -24,35 +17,18 @@ import { listAnimation } from './list-animation';
   templateUrl: './battlefield.component.html',
   styleUrls: ['./battlefield.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [listAnimation],
 })
 export class BattlefieldComponent {
-  allies = computed(() =>
-    this.characterService.characters().filter(c => c.type === 'ally')
-  );
-  enemies = computed(() =>
-    this.characterService.characters().filter(c => c.type === 'enemy')
-  );
-
-  animationTrigger = signal(0);
+  allies = computed(() => this.characterService.getEnemies());
+  enemies = computed(() => this.characterService.getEnemies());
 
   constructor(public characterService: CharacterService) {}
 
   onCharacterDelete(id: string) {
     this.characterService.deleteCharacter(id);
-    this.triggerAnimation();
   }
 
   sortCharacters() {
     this.characterService.sortCharactersByInitiative();
-    this.triggerAnimation();
-  }
-
-  trackByFn(index: number, character: Character) {
-    return character.id;
-  }
-
-  triggerAnimation() {
-    this.animationTrigger.update(v => v + 1);
   }
 }
