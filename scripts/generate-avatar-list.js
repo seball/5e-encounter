de4ddcd2f74cdd5d24f7cd2eb62ac5f50c03a92e
S3ball <sebastian.czubienko@gmail.coml>
2024-12-01 22:25:39 +0100

    script fix, css adjustment

--- scripts/generate-avatar-list.js
+++ scripts/generate-avatar-list.js
@@ -3,39 +3,31 @@
 const fs = require('fs');
 const path = require('path');
 
+function processDirectory(inputDir, outputPath, type) {
+  fs.readdir(inputDir, (err, files) => {
+    if (err) {
+      console.error(`Error reading ${type} directory:`, err);
+      return;
+    }
+
+    const imageFiles = files.filter(
+      file =>
+        /\.(jpg|jpeg|png|gif|webp)$/i.test(file) && !file.endsWith('.json')
+    );
+
+    const avatarList = {
+      avatars: imageFiles,
+    };
+
+    fs.writeFileSync(outputPath, JSON.stringify(avatarList, null, 2));
+    console.log(`${type} list generated successfully!`);
+  });
+}
+
 const portraitsDir = path.join(__dirname, '../src/assets/portraits');
-let outputFile = path.join(__dirname, '../src/assets/portraits.json');
+const portraitsOutput = path.join(__dirname, '../src/assets/portraits.json');
+processDirectory(portraitsDir, portraitsOutput, 'Portraits');
 
-fs.readdir(portraitsDir, (err, files) => {
-  if (err) {
-    console.error('Error reading portraits directory:', err);
-    return;
-  }
-  const imageFiles = files.filter(
-    file =>
-      /\.(jpg|jpeg|png|gif|webp)$/i.test(file) && file !== 'portraits.json'
-  );
-  const avatarList = {
-    avatars: imageFiles,
-  };
-  fs.writeFileSync(outputFile, JSON.stringify(avatarList, null, 2));
-  console.log('Avatar list generated successfully!');
-});
-
-// const monstersDir = path.join(__dirname, '../src/assets/monsters');
-// outputFile = path.join(__dirname, '../src/assets/monsters.json');
-
-// fs.readdir(monstersDir, (err, files) => {
-//   if (err) {
-//     console.error('Error reading monsters directory:', err);
-//     return;
-//   }
-//   const imageFiles = files.filter(
-//     file => /\.(jpg|jpeg|png|gif|webp)$/i.test(file) && file !== 'monsters.json'
-//   );
-//   const avatarList = {
-//     avatars: imageFiles,
-//   };
-//   fs.writeFileSync(outputFile, JSON.stringify(avatarList, null, 2));
-//   console.log('Avatar list generated successfully!');
-// });
+const monstersDir = path.join(__dirname, '../src/assets/monsters');
+const monstersOutput = path.join(__dirname, '../src/assets/monsters.json');
+processDirectory(monstersDir, monstersOutput, 'Monsters');
